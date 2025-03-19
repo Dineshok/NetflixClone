9fa142af92457e0ea397f576fda525ea05609e88
Dineshok <dhineshok@gmail.com>
2025-03-19 17:20:13 +0530

    Updated basename

--- src/index.js
+++ src/index.js
@@ -7,12 +7,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <Router basename="/NetflixClone">
     <App />
   </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
